d59c770c000456dfbb9b37f5f72a32709e06a838
Dodanim <kttim@hotmail.com>
2025-08-23 13:02:21 +0100

    import libraries

---
+++ librerias.py
@@ -0,0 +1,7 @@
+from random import randint
+from random import choice
+randomNumber = randint(1,10)
+print("Random number between 1-10: ", randomNumber)
+
+fruits= ["Bananas", "Watermelon", "Apples","Coconut"]
+print("Random fruit: ",choice(fruits))
