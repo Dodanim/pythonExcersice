0c7e7cc651cc5249a429b8d2358ab13f27730ccb
Dodanim <kttim@hotmail.com>
2025-08-23 12:43:54 +0100

    excersice 1 python IDCL, practice 2

---
+++ guess.py
@@ -0,0 +1,13 @@
+noOfBananas = 10
+
+noOfCoconuts = 12
+
+noOfKiwis = 20
+
+noOfPineapples = 13
+
+noOfExoticFruits = noOfBananas + noOfCoconuts + noOfKiwis + noOfPineapples
+
+print('Sunny Side Fruit Imports / Exports PLC')
+
+print('Total pieces of fruit in stock: ' + noOfExoticFruits)
